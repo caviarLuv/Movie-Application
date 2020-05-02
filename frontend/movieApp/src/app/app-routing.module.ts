918e489aed70549ef823b043004c4579decad0af
Ahsa Yakir <ahsayakir.yakir@sjsu.edu>
2020-05-02 13:24:13 -0700

    more functionality

--- frontend/movieApp/src/app/app-routing.module.ts
+++ frontend/movieApp/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MovieListComponent } from './movielist/movielist.component';
 import { MovieComponent } from './movie/movie.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
   { path: '', component: MovieListComponent},
   { path: 'movies', component: MovieListComponent},
-  { path: 'movie/:movieId', component: MovieComponent}
+  { path: 'movie/:movieId', component: MovieComponent},
+  { path: 'profile/:userId', component: ProfileComponent }
 ];
 
 @NgModule({
--- frontend/movieApp/src/app/header/header.component.ts
+++ frontend/movieApp/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  viewProfile() {
+    this.router.navigate(['/profile/' + '23']);
+  }
+}
