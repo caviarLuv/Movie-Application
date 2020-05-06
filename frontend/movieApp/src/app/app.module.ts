d5145ce00f2c3fc00da66b2fe0db7a83e09f2cc4
caviarLuv <yashleycaviar@gmail.com>
2020-05-05 22:50:40 -0700

    comment and rating

--- frontend/movieApp/src/app/app.module.ts
+++ frontend/movieApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -62,7 +63,8 @@ import { AppRoutingModule } from './app-routing.module';
     MatCardModule,
     MatProgressSpinnerModule,
     MatSelectModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatSliderModule 
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
